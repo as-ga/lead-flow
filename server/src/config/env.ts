71499ebde99c09daaf694b34449811af700f5430
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2026-05-17 13:22:32 +0530

    feat: add environment variables for db and jwt conf

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -3,7 +3,15 @@ dotenv.config();
 
 const _env = {
   port: Number(process.env.PORT) || 5000,
+  isDev: process.env.NODE_ENV?.toLocaleLowerCase() === "development",
+  clientURL: process.env.CLIENT_URL!,
+
+  //  Database
   mongodbURI: process.env.MONGODB_URI!,
+
+  // JWT related
+  jwtSecret: process.env.JWT_SECRET!,
+  jwtExpires: Number(process.env.JWT_EXPIRE) || 1 * 24 * 60 * 60, // default to 1 days in seconds
 };
 
 const env = Object.freeze(_env);
