67956a8601ed1e294176703b4a0dec2aea7990d4
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2026-05-18 21:23:06 +0530

    fix: change HTTP method for update lead route from GET to PUT

--- server/src/modules/lead/lead.routes.ts
+++ server/src/modules/lead/lead.routes.ts
@@ -4,7 +4,7 @@ import * as lead from "./lead.controllers";
 const router: Router = Router();
 
 router.post("/create", lead.createLead);
-router.get("/update/:id", lead.updateLead);
+router.put("/update/:id", lead.updateLead);
 router.delete("/delete/:id", lead.deleteLead);
 router.get("/get/:id", lead.getLeadByID);
 router.get("/get", lead.getLeads);
